678119389ee8aa5ac1fd5a330a066a23ca659f15
giwilorjelly <gwlgidwani@gmail.com>
2020-04-28 22:37:03 +0530

    added search feature

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 import os
-
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,7 +22,7 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", message="your search results will appear here")
 
 @app.route("/sign_in")
 def sign_in():
@@ -32,3 +31,21 @@ def sign_in():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/search",methods=["GET"])
+def search():
+    if not request.args.get("book"):
+        return render_template("index.html",message="err: empty search query")
+
+    #capitalizing query and adding wild character
+    query = "%" + request.args.get("book") + "%"
+    query = query.title()
+
+    results = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn LIKE :query OR title LIKE :query OR author LIKE :query LIMIT 20",{"query":query})
+
+    if results.rowcount == 0:
+        return render_template("index.html",message="err: no books found")
+
+    books = results.fetchall()
+
+    return render_template("index.html",books=books)
